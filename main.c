a4930f937f21bc0c043d8a486b9c5ca8f170a34f
Pedro Marçal Ballesteros <54600506+Netroxx@users.noreply.github.com>
2024-04-22 11:36:35 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+  // Abre o arquivo para escrita
+  FILE *arquivo = fopen("exemplo.txt", "w");
+
+  // Verifica se o arquivo foi aberto com sucesso para escrita
+  if (arquivo != NULL) {
+    fprintf(arquivo, "Hello, World\n");
+    // Fecha o arquivo de escrita
+    fclose(arquivo);
+    printf("Texto gravado com sucesso.\n");
+  } else {
+    // Se houver erro na abertura do arquivo para escrita
+    printf("Erro ao abrir o arquivo para escrita.\n");
+    return 1; // Retorna um código de erro
+  }
+
+  // Abre o arquivo para leitura
+  arquivo = fopen("exemplo.txt", "r");
+
+  // Verifica se o arquivo foi aberto com sucesso para leitura
+  if (arquivo != NULL) {
+    char linha[100]; // Define um buffer para armazenar a linha lida
+
+    // Lê e imprime cada linha do arquivo
+    while (fgets(linha, sizeof(linha), arquivo) != NULL) {
+      printf("Conteudo lido do arquivo: %s", linha);
+    }
+    // Fecha o arquivo de leitura
+    fclose(arquivo);
+  } else {
+    // Se houver erro na abertura do arquivo para leitura
+    printf("Erro ao abrir o arquivo para leitura.\n");
+    return 1; // Retorna um código de erro
+  }
+
+  return 0;
+}
