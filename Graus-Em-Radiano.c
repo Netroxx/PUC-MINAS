80337c92fc62a695b217b80ba45ee73cd8b7acee
Pedro Marçal Ballesteros <54600506+Netroxx@users.noreply.github.com>
2024-04-22 11:43:47 -0300

    Add files via upload

---
+++ Graus-Em-Radiano.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void calcularad();
+void calcularad(){
+int ang;
+float pi, rad;
+  pi = 3,14
+    printf("converte graus em radiano \n");
+  printf("Angulo:");
+
+}
+int main(){
+void calcularad();
+  return 0
+}
